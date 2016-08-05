9c66d1485b71296c08cfb8dc35228ede84283808
mcasciato <michael.casciato@icloud.com>
2016-08-05 16:26:54 -0400

    ceu model associations

--- app/models/ceu.rb
+++ app/models/ceu.rb
@@ -1,2 +1,7 @@
 class Ceu < ActiveRecord::Base
+    belongs_to :user
+    has_many :ceu_certificates
+    has_many :certificates, through: :ceu_certificates
+
+    validates :name, :date, :location, :duration, presence: true
 end
