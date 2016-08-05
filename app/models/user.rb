87cf64079ef8fc7c35e0680c0002dc6fd4dab71d
mcasciato <michael.casciato@icloud.com>
2016-08-05 16:24:55 -0400

    user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :email, presence: true, uniqueness: true
+
+  has_many :ceus
+  has_many :ceu_certificates, through: :ceus
+  has_many :certificates, through: :ceus
 end
