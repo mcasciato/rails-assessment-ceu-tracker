f68fe140339745fd63c9c3f1221a1188c0073d96
mcasciato <michael.casciato@icloud.com>
2016-08-06 14:32:56 -0400

    began implemention of omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/facebook/callback' => 'sessions#create'
+
 end
