527e8f429d3a97b46350a1c7b138bc83785ce99a
mcasciato <michael.casciato@icloud.com>
2016-08-08 10:52:02 -0400

    added index action for ceu

--- app/controllers/ceus_controller.rb
+++ app/controllers/ceus_controller.rb
@@ -1,5 +1,9 @@
 class CeusController < ApplicationController
   def index
-
+    if params[:user_id]
+      @ceus = User.find(params[:user_id]).ceus
+    else
+      @ceus = Ceu.all
+    end
   end
 end
