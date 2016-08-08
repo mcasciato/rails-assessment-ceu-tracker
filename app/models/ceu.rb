1b55ac1cb68c0cf922bc15c87bdc5df58b9e20e4
mcasciato <michael.casciato@icloud.com>
2016-08-08 17:55:40 -0400

    new ceu form mostly done--working on certificate entry

--- app/models/ceu.rb
+++ app/models/ceu.rb
@@ -6,4 +6,10 @@ class Ceu < ActiveRecord::Base
 
     # accepts_nested_attributes_for :certificates, reject_if: :all_blank
 
+    def certificate_attributes=(certificate_attributes)
+      certificate_attributes.values.each do |certificate_attribute|
+        certficate = Certificate.find_or_create_by(certificate_attribute)
+        self.certificates << certficate
+      end
+    end
 end
