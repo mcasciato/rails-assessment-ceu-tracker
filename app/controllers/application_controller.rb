99b43a60009a5dec111cc4b472aa78b0d146001d
mcasciato <michael.casciato@icloud.com>
2016-08-23 10:00:03 -0400

    fixed reroute after login/signup to users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path #need to change this?
+    request.env['omniauth.origin'] || user_path(current_user) #need to change this?
   end
 
 end
