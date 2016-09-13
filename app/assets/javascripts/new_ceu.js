3b60f40cff1a1395764ef0de2be993fa087c9e12
mcasciato <michael.casciato@icloud.com>
2016-09-13 17:24:11 -0400

    working on submitForm for new_ceu.js

--- app/assets/javascripts/new_ceu.js
+++ app/assets/javascripts/new_ceu.js
@@ -4,5 +4,10 @@ $(document).on('turbolinks:load', function() {
 })
 
 function submitForm() {
-
+  $('#new_ceu').submit(function(event){
+    event.preventDefault();
+    var values = $(this).serialize();
+    var submitting = $.post('/ceus', values);
+    
+  })
 }
