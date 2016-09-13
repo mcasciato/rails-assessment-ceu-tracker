9661eb2a44c49d00ed71169f5dd5693d72306960
mcasciato <michael.casciato@icloud.com>
2016-09-13 17:32:33 -0400

    finished submitForm function

--- app/assets/javascripts/new_ceu.js
+++ app/assets/javascripts/new_ceu.js
@@ -8,6 +8,11 @@ function submitForm() {
     event.preventDefault();
     var values = $(this).serialize();
     var submitting = $.post('/ceus', values);
-    
+    submitting.done(function(data){
+      var newCeu = new Ceu(data["title"], data["date"], data["location"], data["duration"], data["id"]);
+      $('#show').show();
+      $('#ceuDetails').text("CEU Details");
+      newCeu.createHTML();
+    })
   })
-}
+};
