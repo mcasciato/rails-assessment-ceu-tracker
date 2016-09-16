6c9a5e39ec05f19f4607da9b5f4927665716c67f
mcasciato <michael.casciato@icloud.com>
2016-09-16 16:10:04 -0400

    moved ceu JS to show from _ceu partial; added notes back to ceu#show; added notes to ceu serializer for JSON

--- app/serializers/ceu_serializer.rb
+++ app/serializers/ceu_serializer.rb
@@ -1,4 +1,5 @@
 class CeuSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :location, :duration, :certificate
   has_many :users
+  has_many :notes
 end
--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,5 @@
 class NoteSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :content
   has_one :user, serializer: NoteUserSerializer
   has_one :ceu, serializer: NoteCeuSerializer
 end
