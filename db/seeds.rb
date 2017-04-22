6d113e866c68222040f665f151041bf01cf724f0
mcasciato <michael.casciato@icloud.com>
2017-04-22 16:14:38 -0400

    created some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Users
+User.create(
+    name: Faker::Name.name,
+    occupation: "Occupational Therapist",
+    email: Faker::Internet.email,
+    password: 'password1',
+    password_confirmation: 'password1'
+)
+
+User.create(
+    name: Faker::Name.name,
+    occupation: "Recreational Therapist",
+    email: Faker::Internet.email,
+    password: 'password2',
+    password_confirmation: 'password2'
+)
+
+User.create(
+    name: Faker::Name.name,
+    occupation: "Physical Therapist",
+    email: Faker::Internet.email,
+    password: 'password3',
+    password_confirmation: 'password3'
+)
+
+User.create(
+    name: "Michael Casciato",
+    occupation: "Recreational Therapist",
+    email: "casciatom@gmail.com",
+    password: 'password21',
+    password_confirmation: 'password21',
+    admin: true
+)
+
+# CEUs
+15.times do
+    Ceu.create(
+        title: Faker::Educator.course,
+        date: Faker::Time.between(3.years.ago, Date.today, :all),
+        duration: Faker::Number.between(30, 300),
+        location: Faker::StarWars.planet,
+        certificate: "National"
+    )
+end
+
+10.times do
+    Ceu.create(
+        title: Faker::Educator.course,
+        date: Faker::Time.between(3.years.ago, Date.today, :all),
+        duration: Faker::Number.between(30, 300),
+        location: Faker::StarWars.planet,
+        certificate: "State"
+    )
+end
+
+# Notes
+15.times do
+    Note.create(
+        content: Faker::Hipster.paragraph,
+        user_id: Faker::Number.between(1,4),
+        ceu_id: Faker::Number.between(1,10)
+    )
+end
